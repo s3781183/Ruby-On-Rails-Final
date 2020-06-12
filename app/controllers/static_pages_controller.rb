1121d86eb9a2bdf3606ab255e6b213c1ec26ae46
Ruchelle Balasuriya <rushebala@gmail.com>
2020-06-13 01:41:31 +1000

    home

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
     def home
-        
+        @Sri_Time = Time.use_zone('Sri Jayawardenepura') { Time.current }
+        @Hong_Time =  Time.use_zone('Sapporo') { Time.current }
     end
 
 end
